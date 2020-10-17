5e53b62cdefea7d5c7192e652e6c973b8eeca9fd
Martin René Sørensen <git@sicet7.com>
2020-10-17 02:28:29 +0200

    Fixed ranger alias. Made installer install fisher. Ignored installed fisher from git.

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,6 +1,6 @@
 # General
 alias mv="mv -iv"
 alias rm="rm -v"
-alias ranger="rg --choosedir=$HOME/.rangerdir; cd (cat $HOME/.rangerdir)"
+alias rg="ranger --choosedir=$HOME/.rangerdir; cd (cat $HOME/.rangerdir)"
 alias df="df -h"
 alias du="du -hs"
--- install.sh
+++ install.sh
@@ -2,22 +2,26 @@
 
 SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )";
 
-which=$(which git);
-if [ ${#which} -lt 1 ]; then
-    echo "Missing access to \"git\" command.";
-    exit 1;
-fi
+installIfMissing()
+{
+    PACKAGE=$1
+    COMMAND=$2
+    which=$(which "$COMMAND");
+    if [ ${#which} -lt 1 ]; then
+        sudo apt install "$PACKAGE" -y
+    fi
+}
 
 linkDotFile()
 {
     INPUT=$1
     OUTPUT=$2
-    if [ -L "$INPUT" ]; then
-        echo "Input cannot be a symlink!"
-        exit 1;
-    fi
     if [ ! -f "$INPUT" ] && [ ! -d "$INPUT" ]; then
         echo "Input not found: $INPUT"
+        exit 1;
+    fi
+    if [ -L "$INPUT" ]; then
+        echo "Input cannot be a symlink!"
         exit 1;
     fi
     if [ -f "$OUTPUT" ] || [ -d "$OUTPUT" ]; then
@@ -31,15 +35,45 @@ linkDotFile()
     if [ ! -d "$OUTPUT_DIR" ]; then
         mkdir -p "$OUTPUT_DIR"
     fi
-    ln -s "$INPUT" "$OUTPUT"
+    ln -s "$INPUT" "$OUTPUT" && echo "linked: $OUTPUT -> $INPUT"
 }
 
-if [ -d "$SCRIPT_PATH/tmux/.tmux/plugins/tpm" ]; then
-    rm -rf "$SCRIPT_PATH/tmux/.tmux/plugins/tpm" && echo "removed: '$SCRIPT_PATH/tmux/.tmux/plugins/tpm'"
+#Install Dependencies from apt
+installIfMissing "git" "git"
+installIfMissing "fish" "fish"
+installIfMissing "ranger" "ranger"
+installIfMissing "tmux" "tmux"
+installIfMissing "bash" "bash"
+installIfMissing "wget" "wget"
+
+#Download TPM
+TPM_DIR="$SCRIPT_PATH/tmux/.tmux/plugins/tpm"
+if [ -d "$TPM_DIR" ]; then
+    rm -rf "$TPM_DIR" && echo "removed: '$TPM_DIR'"
 fi
+git clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
 
-git clone https://github.com/tmux-plugins/tpm "$SCRIPT_PATH/tmux/.tmux/plugins/tpm"
+#Download Fisher
+FISHER_FILE="$SCRIPT_PATH/fish/functions/fisher.fish"
+if [ ! -d $(dirname "$FISHER_FILE") ]; then
+    mkdir -p $(dirname "$FISHER_FILE")
+fi
+if [ -f "$FISHER_FILE" ]; then
+    rm -fv "$FISHER_FILE"
+fi
+wget https://git.io/fisher --output-document="$FISHER_FILE"
 
+#TMUX Setup
 linkDotFile "$SCRIPT_PATH/tmux/.tmux.conf" "$HOME/.tmux.conf"
 linkDotFile "$SCRIPT_PATH/tmux/.tmux" "$HOME/.tmux"
+
+#FISH Setup
 linkDotFile "$SCRIPT_PATH/fish/config.fish" "$HOME/.config/fish/config.fish"
+linkDotFile "$SCRIPT_PATH/fish/aliases.fish" "$HOME/.config/fish/aliases.fish"
+linkDotFile "$FISHER_FILE" "$HOME/.config/fish/functions/fisher.fish"
+
+#Run TPM plugin installer
+bash ~/.tmux/plugins/tpm/bin/install_plugins
+
+#Run Fisher Plugin installer
+fish -c fisher
