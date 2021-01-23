1e5ccee3b57c9ffe6a3e6905e7b7df43041290b5
Martin René Sørensen <git@sicet7.com>
2021-01-23 23:39:44 +0100

    Fixed starship init statement.

--- installers/starship.sh
+++ installers/starship.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 curl -fsSL https://starship.rs/install.sh | bash -s -- --yes && \
-if [ \( -f "$HOME/.bashrc" \) -a \( "$(grep -i 'starship init bash' ~/.bashrc | wc -l)" = "0" \) ]; then echo "eval \"\$\(starship init bash\)\"" >> "$HOME/.bashrc"; fi && \
+if [ \( -f "$HOME/.bashrc" \) -a \( "$(grep -i 'starship init bash' ~/.bashrc | wc -l)" = "0" \) ]; then echo "eval \"\$(starship init bash)\"" >> "$HOME/.bashrc"; fi && \
 echo "Installed Starship prompt."
