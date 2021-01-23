906f0df6622ff6d0a1310fa78103fedf3397782e
Martin René Sørensen <git@sicet7.com>
2021-01-23 17:15:35 +0100

    Made fisher installer update dependencies.

--- installers/fisher.sh
+++ installers/fisher.sh
@@ -4,4 +4,5 @@ sudo apt install -y curl && \
 mkdir -p "$HOME/.config/fish/functions" && \
 wget https://git.io/fisher --output-document="$HOME/.config/fish/functions/fisher.fish" && \
 cp -v "$SETUP_ROOT/config/fish/fish_plugins" ~/.config/fish/fish_plugins && \
+fish -c "fisher update" && \
 echo "Installed Fisher"
