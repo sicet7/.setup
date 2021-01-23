aa81f9b951d215bf5afcf054715075e9344a1ecc
Martin René Sørensen <git@sicet7.com>
2021-01-23 20:43:43 +0100

    Added tpm check before adding config to tmux.conf

--- installers/tpm.sh
+++ installers/tpm.sh
@@ -3,7 +3,7 @@ cd "$SETUP_BUILD" && \
 rm -rf "$HOME/.tmux/plugins/tpm" && \
 mkdir -p "$HOME/.tmux/plugins" && \
 git clone --depth="1" --branch="master" https://github.com/tmux-plugins/tpm.git "$HOME/.tmux/plugins/tpm" && \
-cat "$SETUP_ROOT/config/tpm.conf" >> "$HOME/.tmux.conf" && \
+if [ \( ! -f "$HOME/.tmux.conf" \) -o \( "$(grep -i '044991c8a912ff3c9b0de5652ffd7efd59a96836c03d7484064267f117bfd6e9' ~/.tmux.conf | wc -l)" = "0" \) ]; then cat "$SETUP_ROOT/config/tpm.conf" >> "$HOME/.tmux.conf"; fi && \
 export TMUX_PLUGIN_MANAGER_PATH="$HOME/.tmux/plugins" && \
 bash ~/.tmux/plugins/tpm/bin/install_plugins && \
 echo "Installed Tmux Plugin Manager"
