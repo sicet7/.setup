19c41ff57b26d46c5ae69d2dfae935fbcc9b8e35
Martin René Sørensen <git@sicet7.com>
2021-01-23 18:14:25 +0100

    Fixed tmux install and config.

--- installers/tmux.sh
+++ installers/tmux.sh
@@ -2,7 +2,7 @@
 cd "$SETUP_BUILD" && \
 git clone --depth="1" --branch="3.1c" https://github.com/tmux/tmux.git && \
 cd tmux && \
-sudo apt install autoconf automake pkg-config && \
+sudo apt install -y autoconf automake pkg-config && \
 sh ./autogen.sh && \
 ./configure && \
 make && \
