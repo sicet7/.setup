19ca6ed599167984a69bb76d9f5de601f450dd3f
Martin René Sørensen <git@sicet7.com>
2021-01-23 17:08:30 +0100

    Fixed fisher dependency file.

--- installers/fisher.sh
+++ installers/fisher.sh
@@ -3,5 +3,5 @@ sudo apt update && \
 sudo apt install -y curl && \
 mkdir -p "$HOME/.config/fish/functions" && \
 wget https://git.io/fisher --output-document="$HOME/.config/fish/functions/fisher.fish" && \
-cp -v "$SETUP_ROOT/config/fish/fishfile" ~/.config/fish/fishfile && \
+cp -v "$SETUP_ROOT/config/fish/fish_plugins" ~/.config/fish/fish_plugins && \
 echo "Installed Fisher"
