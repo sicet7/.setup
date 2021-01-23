87c3fd453d4bb38fca78e7005c82dc9fe63759f7
Martin René Sørensen <git@sicet7.com>
2021-01-23 22:14:27 +0100

    Testing gitconfig

--- installers/gitconfig.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo "Starting gitconfig setup." && \
-read -p"Please enter name: " name </dev/tty && \
-read -p"Please enter email: " email </dev/tty && \
-echo "[user]" > "$HOME/.gitconfig" && \
-echo "    email = $email" >> "$HOME/.gitconfig" && \
-echo "    name = $name" >> "$HOME/.gitconfig" && \
-cat "$SETUP_ROOT/config/gitconfig" >> "$HOME/.gitconfig" && \
-echo "Finished setting up .gitconfig"
