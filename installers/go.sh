d5826bf24b7e46e323c0df5bf855ec3d83cdd087
Martin René Sørensen <git@sicet7.com>
2021-01-24 03:27:31 +0100

    Updated go installer to compile from source.

--- installers/go.sh
+++ installers/go.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
-#sudo apt install build-essential && \
-#git clone --depth="1" --branch="go1.15.7" https://github.com/golang/go.git && \
-#cd "go/src" && \
-#sudo bash ./all.bash && \
-#echo "Installed GO"
-
-
-sudo apt install golang && \
+cd "$SETUP_BUILD" && \
+sudo apt install build-essential golang && \
+git clone --depth="1" --branch="go1.15.7" https://github.com/golang/go.git && \
+cd "$SETUP_BUILD/go/src" && \
+bash ./all.bash && \
+sudo apt remove golang && \
+sudo apt autoremove && \
+cp -rv "$SETUP_BUILD/go/bin/." "$HOME/.local/bin/" && \
 export GOPATH="$HOME/.go" && \
 if [ \( -f "$HOME/.bashrc" \) -a \( "$(grep -i 'export GOPATH=' ~/.bashrc | wc -l)" = "0" \) ]; then echo "export GOPATH=\$HOME/.go" >> "$HOME/.bashrc"; fi && \
 if [ \( -f "$HOME/.bashrc" \) -a \( "$(grep -i 'GOPATH/bin' ~/.bashrc | wc -l)" = "0" \) ]; then echo "export PATH=\$PATH:\$GOPATH/bin" >> "$HOME/.bashrc"; fi && \
+if [ \( -f "$HOME/.bashrc" \) -a \( "$(grep -i 'HOME/.local/bin' ~/.bashrc | wc -l)" = "0" \) ]; then echo "export PATH=\$PATH:\$HOME/.local/bin" >> "$HOME/.bashrc"; fi && \
 if [ \( -f "$HOME/.zshrc" \) -a \( "$(grep -i 'export GOPATH=' ~/.zshrc | wc -l)" = "0" \) ]; then echo "export GOPATH=\$HOME/.go" >> "$HOME/.zshrc"; fi && \
 if [ \( -f "$HOME/.zshrc" \) -a \( "$(grep -i 'GOPATH/bin' ~/.zshrc | wc -l)" = "0" \) ]; then echo "export PATH=\$PATH:\$GOPATH/bin" >> "$HOME/.zshrc"; fi && \
+if [ \( -f "$HOME/.zshrc" \) -a \( "$(grep -i 'HOME/.local/bin' ~/.zshrc | wc -l)" = "0" \) ]; then echo "export PATH=\$PATH:\$HOME/.local/bin" >> "$HOME/.zshrc"; fi && \
 echo "Installed golang"
