3c1ae36b2873e7b0ed9f40d31f0ec80b8f5845bc
Martin René Sørensen <git@sicet7.com>
2021-01-24 02:14:39 +0100

    Aliases installer fix

--- installers/aliases.sh
+++ installers/aliases.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cp "$SETUP_ROOT/config/aliases" "$HOME/.aliases" && \
-if [ \( -f "$HOME/.bashrc" \) -a \( "$(grep -i '.aliases' ~/.bashrc | wc -l)" = "0" \) ]; then echo "if [ -f ~/.aliases ]; then . ~/.aliases; fi" >> "$HOME/.bashrc"; fi && \
+if [ \( -f "$HOME/.bashrc" \) -a \( "$(grep -i '-f ~/.aliases' ~/.bashrc | wc -l)" = "0" \) ]; then echo "if [ -f ~/.aliases ]; then . ~/.aliases; fi" >> "$HOME/.bashrc"; fi && \
 if [ \( -f "$HOME/.zshrc" \) -a \( "$(grep -i '.aliases' ~/.zshrc | wc -l)" = "0" \) ]; then echo "source \$HOME/.aliases" >> "$HOME/.zshrc"; fi && \
 echo "Installed aliases"
