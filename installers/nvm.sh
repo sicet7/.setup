da48f4e7aea2119f9ef0b75fd0f87583bd3e53b8
Martin René Sørensen <git@sicet7.com>
2021-01-29 22:59:19 +0100

    Fixed nvm installer.

--- installers/nvm.sh
+++ installers/nvm.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-PROFILE="$SETUP_ROOT/config/sharedrc"
+export PROFILE="$SETUP_ROOT/config/sharedrc"
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 echo "Installed NVM"
