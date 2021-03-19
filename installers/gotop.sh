b5f34318dc60e49507f4935d9c0c123a048cf1b3
Martin René Sørensen <git@sicet7.com>
2021-03-19 21:53:49 +0100

    gotop build test

--- installers/gotop.sh
+++ installers/gotop.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
-go version
-go clean -cache -modcache -i -r
-go get -u github.com/xxxserxxx/gotop/cmd/gotop
+cd "$SETUP_BUILD" || exit 1;
+VERSION="4.1.1";
+git clone --depth="1" --branch="v$VERSION" https://github.com/xxxserxxx/gotop.git
+cd gotop || exit 1;
+sed -i '/^go/d' go.mod
+go build -o gotop \
+	-ldflags "-X main.Version=v${VERSION} -X main.BuildDate=$(date +%Y%m%dT%H%M%S)" \
+	./cmd/gotop
+
+if [ ! -d "$HOME/.local" ]; then
+    mkdir -p "$HOME/.local"
+fi
+
+if [ ! -d "$HOME/.local/bin" ]; then
+    mkdir -p "$HOME/.local/bin"
+fi
+
+exit 1;
+
 echo "Installed gotop"
