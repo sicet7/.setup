00be2d09a3d7497ecbeec85765d4fc245a1a8b36
Martin René Sørensen <sicet7@users.noreply.github.com>
2021-03-19 18:56:47 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ EXEC_PATH="$(cd "$(dirname "$0")" >/dev/null 2>&1; pwd -P)";
 SETUP_ROOT="$EXEC_PATH/.setup";
 sudo add-apt-repository ppa:git-core/ppa && \
 sudo apt update && \
-sudo apt install git && \
+sudo apt install -y git && \
 git clone --depth="1" --branch="master" https://github.com/sicet7/.setup.git $SETUP_ROOT && \
 . "$SETUP_ROOT/run.sh" && \
 rm -rfv "$SETUP_ROOT" && \
