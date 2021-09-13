d95c4cbf049cf6ea978d569a5ab7e58f444facfe
Martin René Sørensen <sicet7@users.noreply.github.com>
2021-09-13 20:14:17 +0200

    Update sharedrc.sh

--- installers/sharedrc.sh
+++ installers/sharedrc.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 cp -v "$SETUP_ROOT/config/sharedrc" "$HOME/.sharedrc"
 if [ \( ! -f "$HOME/.bashrc" \) -o \( "$(grep -i '.sharedrc; fi' ~/.bashrc | wc -l)" = "0" \) ]; then
     echo "if [ -f ~/.sharedrc ]; then . ~/.sharedrc; fi" >> "$HOME/.bashrc";
