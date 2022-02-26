2f3eea4a7924ae0723a15439c6c4cc9e241c3858
Martin René Sørensen <sicet7@users.noreply.github.com>
2022-02-26 11:12:03 +0100

    Update starship.sh

--- installers/starship.sh
+++ installers/starship.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 #wget https://starship.rs/install.sh -O- | sh --yes
-sh -c "$(curl -fsSL https://starship.rs/install.sh) --force"
+sh -c "$(curl -fsSL https://starship.rs/install.sh)" "install.sh" --yes
 echo "Installed Starship prompt."
