913ce0666f86ec53d665fe2c3367fd27de899004
Martin René Sørensen <sicet7@users.noreply.github.com>
2022-09-24 23:29:25 +0200

    Update starship.sh

--- installers/starship.sh
+++ installers/starship.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 set -e
 sh -c "$(curl -fsSL https://starship.rs/install.sh)" "install.sh" --yes
+mkdir -p "$HOME/.config"
+cp -v "$SETUP_ROOT/config/starship.toml" "$HOME/.config/starship.toml"
 echo "Installed Starship prompt."
