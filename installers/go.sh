2517205d27dc1f2507b395f83211ae9ee332c157
Martin René Sørensen <sicet7@users.noreply.github.com>
2023-02-11 23:16:58 +0100

    Update go.sh

--- installers/go.sh
+++ installers/go.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
 set -e
 sudo rm -rf "$HOME/.go"
-mkdir -p "$HOME/.go/path"
-sudo apt install -y build-essential golang
-hash -r
-git clone --depth="1" --branch="go1.20" https://github.com/golang/go.git "$HOME/.go/root"
-cd "$HOME/.go/root/src" || exit 1;
-bash ./all.bash
-sudo apt remove -y golang
-sudo apt autoremove -y
-export GOPATH="$HOME/.go/path"
-export GOROOT="$HOME/.go/root"
-export PATH="$PATH:$HOME/.go/path/bin:$HOME/.go/root/bin"
+sudo rm -rf "/usr/local/go"
+wget "https://go.dev/dl/go1.20.linux-amd64.tar.gz" --output-document="$SETUP_BUILD/go.tar.gz"
+tar -C /usr/local -xzf "$SETUP_BUILD/go.tar.gz"
+export PATH="$PATH:/usr/local/go/bin"
 echo "Installed golang"
