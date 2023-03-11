34ba5f7d22bb877fcb5d451653be24ef1a82731a
Martin René Sørensen <sicet7@users.noreply.github.com>
2023-03-11 13:57:57 +0100

    Update go.sh

--- installers/go.sh
+++ installers/go.sh
@@ -2,7 +2,7 @@
 set -e
 sudo rm -rf "$HOME/.go"
 sudo rm -rf "/usr/local/go"
-wget "https://go.dev/dl/go1.20.linux-amd64.tar.gz" --output-document="$SETUP_BUILD/go.tar.gz"
+wget "https://go.dev/dl/go1.20.2.linux-amd64.tar.gz" --output-document="$SETUP_BUILD/go.tar.gz"
 sudo tar -C /usr/local -xzf "$SETUP_BUILD/go.tar.gz"
 export PATH="$PATH:/usr/local/go/bin"
 echo "Installed golang"
