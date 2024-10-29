3f9620252627d5068fd02ddd1e389db5eeca6e72
Martin René Sørensen <sicet7@users.noreply.github.com>
2024-10-29 22:08:26 +0100

    Update docker.sh

--- installers/docker.sh
+++ installers/docker.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 set -e
 sudo apt remove -y docker docker.io containerd runc
-sudo apt install -y apt-transport-https \
-                    ca-certificates \
-                    curl \
-                    gnupg-agent \
-                    software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt update
-sudo apt install -y docker-ce \
-                    docker-ce-cli \
-                    containerd.io
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
 sudo usermod -aG docker "$USER"
 echo "Installed Docker"
